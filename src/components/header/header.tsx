e51ef4b0d34deea057718f6f865d117ff7747d42
RoomaMaryam <roomamaryam641@gmail.com>
2025-02-03 10:13:18 +0500

    s

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className="col-span-12 bg-gray-100 p-2 flex items-center gap-2">
       <div className="relative h-[70px] w-[60px]">
-        <Image src="/logo.png" alt="logo img " fill sizes="100vw"/>
+        <Image src="/logo.png" alt="logo img " fill sizes="200vw"/>
       </div>
       {/* <h1 className="text-xl font-mono text-green-500">StackOverflow Header </h1> */}
       <NavBar/>
--- src/components/header/search.tsx
+++ src/components/header/search.tsx
@@ -8,7 +8,7 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
+  FormLabel, 
   FormMessage,
 } from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
